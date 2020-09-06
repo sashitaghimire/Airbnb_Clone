12c7b6898c20d5aed4174f83d4abdaf57048e1f5
sashitaghimire <ghimiresashita@gmail.com>
2020-09-06 16:59:11 +0545

    Header Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1> AIRBNB CLONE</h1>
-    </div>
+
+      < Header />
+      < Home />
+{/* Home */}
+      {/* Header */}
+
+      {/*Banner*/}
+           {/* Search*/}
+    
+      {/* Card*/}
+          {/* Footer*/}
+
+    {/* Search Page */}
+          {/* ...... */}
+
+     </div>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+import Banner from './Banner';
+ 
+//ES7 snippets to do 'rfce'
+function Home() {
+    return (
+        <div className="home">
+            
+            <Banner />
+            
+        </div>
+    )
+}
+
+export default Home
