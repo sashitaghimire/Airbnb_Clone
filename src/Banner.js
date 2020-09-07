0acee821fed43540cb6c76b644a89326832fbfa9
sashitaghimire <ghimiresashita@gmail.com>
2020-09-07 15:02:30 +0545

    card added

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Banner.css';
 import {Button} from '@material-ui/core';
+import Search from './Search';
+
 
 function Banner() {
+    const [showSearch, setShowSearch] = useState(false);
     return (
         <div className="banner">
+            <div className="banner__search">
+            {showSearch && <Search /> }
+
+                <Button onClick={()=>setShowSearch(!showSearch)}
+
+                className="banner__searchButton" variant="outlined">{showSearch ? "Hide":"Search Dates"}</Button>
+                
+            </div>
             <div className="banner__info">
                 <h1>Get out and stretch your imagination</h1>
                 <h5>Plan a different kind of getaway to uncover the hidden gems near you.</h5>
--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ function Header() {
     return (
         <div className="header">
             <img 
-            className="header_icon"
+            className="header__icon"
             src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
             alt="airbnb logo"
             />
@@ -19,7 +19,7 @@ function Header() {
         </div >
         <div  className="header__right">
             <p>Become a host</p>
-            <LanguageIcon />
+            <LanguageIcon  />
             <ExpandMoreIcon />
             <Avatar />
         </div>
